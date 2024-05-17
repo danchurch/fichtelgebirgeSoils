37fa2ff7391b5462c543da6d8214ce437402dbcd
danchurch <danchurchthomas@gmail.com>
2024-05-17 15:00:21 +0200

    model for a weekend

---
+++ spatialAnalysis/runSample.r
@@ -0,0 +1,16 @@
+library(Hmsc)
+load("m_hurdleSpatial.rda")
+
+thin = 5
+samples = 1000
+nChains = 3
+nP = 8
+m_hurdleSpatial = sampleMcmc(m_hurdleSpatial, 
+                     samples = samples, 
+                     thin=thin, 
+                     transient = ceiling(0.5*samples*thin), 
+                     nChains = nChains, 
+                     nParallel = nP,
+                     verbose=1)
+
+save(m_hurdleSpatial, file="m_hurdleSpatial_sampled.rda")

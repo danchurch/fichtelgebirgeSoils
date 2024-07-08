c181dccf636db77072e7a80369f5725cee65f30a
danchurch <danchurchthomas@gmail.com>
2024-07-08 08:06:39 +0000

    synch_78_on

---
+++ spatialAnalysis/m_fullModel_logNorm_sampling.r
@@ -0,0 +1,18 @@
+library(Hmsc)
+print("starting model:")
+print("m_fullModel_logNorm")
+print(paste("start time is", Sys.time()))
+load("m_fullModel_logNorm.rda")
+thin = 20
+samples = 1000
+nChains = 4
+nP = 2
+m_fullModel_logNorm = sampleMcmc(m_fullModel_logNorm,
+                      samples = samples, 
+                      thin=thin, 
+                      transient = ceiling(0.5*samples*thin), 
+                      nChains = nChains, 
+                      nParallel = nP
+                     )
+save(m_fullModel_logNorm, file="m_fullModel_logNorm_sampled.rda")
+print(paste("finish time is", Sys.time()))

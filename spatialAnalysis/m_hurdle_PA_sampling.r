2883ec2e986315cb3a639044f8d19154b20c4837
danchurch <danchurchthomas@gmail.com>
2024-07-24 13:05:39 +0000

    sinchDenb724

---
+++ spatialAnalysis/m_hurdle_PA_sampling.r
@@ -0,0 +1,21 @@
+library(Hmsc)
+print("starting model:")
+print("m_hurdle_PA")
+print(paste("start time is", Sys.time()))
+load("m_hurdle_PA.rda")
+thin = 20
+samples = 1000
+nChains = 4
+nP = 2
+#m_hurdle_PA = sampleMcmc(m_hurdle_PA,
+m_hurdle_PA2 = sampleMcmc(m_hurdle_PA,
+                     samples = samples, 
+                     thin=thin, 
+                     transient = ceiling(0.5*samples*thin), 
+                     nChains = nChains, 
+                     nParallel = nP
+                     )
+save(m_hurdle_PA2, file="m_hurdle_PA_sampled2.rda")
+#save(m_hurdle_PA, file="m_hurdle_PA_sampled.rda")
+print(paste("finish time is", Sys.time()))
+
